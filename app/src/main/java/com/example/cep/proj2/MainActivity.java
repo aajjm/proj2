f14d8be55ffea572f3071cbd8ce91830f19afba4
arianxa <pro.arantxa.ordoyo@gmail.com>
2019-02-28 14:20:04 +0100

    holaa

--- app/src/main/java/com/example/cep/proj2/MainActivity.java
+++ app/src/main/java/com/example/cep/proj2/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.cep.proj2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -14,4 +18,38 @@ public class MainActivity extends AppCompatActivity
 
 		// ESTA ES LA RAMA DE ARANTXA
 	}
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+
+
+			inflater.inflate(R.menu.menu, menu);
+
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+
+	// Creamos otro boolean con las opciones del menu.
+	public boolean onOptionsItemSelected(MenuItem item) {
+		boolean devolver;
+
+
+			switch (item.getItemId()) {
+				case R.id.LoginToolbar:
+					Intent menu=new Intent(MainActivity.this,Login.class);
+					startActivityForResult(menu,0);
+					devolver=true;
+					break;
+
+				default:
+					devolver=super.onOptionsItemSelected(item);
+					break;
+
+			}
+			return devolver;
+		}
+
+
+
 }
+
