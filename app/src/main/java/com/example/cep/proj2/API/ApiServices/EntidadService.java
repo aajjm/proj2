83119d3234b921840b344f22cc8bf9cf4aa3e5e3
arianxa <pro.arantxa.ordoyo@gmail.com>
2019-03-22 12:12:02 +0100

    hgdhgdnghd

--- app/src/main/java/com/example/cep/proj2/API/ApiServices/EntidadService.java
+++ app/src/main/java/com/example/cep/proj2/API/ApiServices/EntidadService.java
@@ -1,7 +1,35 @@
 package com.example.cep.proj2.API.ApiServices;
 
+import com.example.cep.proj2.Clases.ClaseEntidad;
+
+import java.util.List;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Response;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 public interface EntidadService {
+    @GET("api/ENTIDAD")
+    Call<List<ClaseEntidad>> getEntidad();
 
+
+    @GET("api/ENTIDAD/{id}")
+    Call <ClaseEntidad>getEntidadId(@Path("id")int id);
+
+
+    @GET("api/ENTIDAD/nombre/{nombre}")
+    Call <ClaseEntidad>getEntidadNombre(@Path("nombre")String nombre);
+
+
+    @POST("api/ENTIDAD")
+    Call <ClaseEntidad> InsertarEntidad(@Body ClaseEntidad entidad);
+
+
+    @DELETE("api/ENTIDAD")
+    Call<ResponseBody> deleteEntidad(@Path("id") int id);
 }
