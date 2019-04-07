4c0b3ba3adf74e137c4dd5dff2bcd9f6cbb9bfe0
Mar <mar.badia215@gmail.com>
2019-04-07 20:01:44 +0200

    fragmentsActividades

--- app/src/main/java/com/example/cep/proj2/Activities/Actividades.java
+++ app/src/main/java/com/example/cep/proj2/Activities/Actividades.java
@@ -1,6 +1,124 @@
 package com.example.cep.proj2.Activities;
 
-public class Actividades {
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import com.example.cep.proj2.Fragments.actividades_assignadas;
+import com.example.cep.proj2.Fragments.actividades_demandadas;
+import com.example.cep.proj2.R;
 
 
+public class Actividades extends AppCompatActivity implements ActionBar.TabListener, ViewPager.OnPageChangeListener {
+
+
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+
+    private ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actividades);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
+
+
+    }
+
+    // Métodos de la interfaz ActionBar.TabListener
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+        mViewPager.setCurrentItem(tab.getPosition());
+    }
+
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+    }
+
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+    }
+
+    // Métodos de la interfaz ViewPager.OnPageChangeListener
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+        getSupportActionBar().setSelectedNavigationItem(position);
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+    }
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+
+            Fragment tabFragment = null;
+
+            switch (position){
+                case 0:
+                    tabFragment = new actividades_assignadas();
+                    break;
+                case 1:
+                    tabFragment = new actividades_demandadas();
+                    break;
+            }
+            return tabFragment;
+        }
+
+        //--------CANTIDAD DE PESTAÑAS--------//
+        @Override
+        public int getCount() {
+            // Show 2 total pages.
+            return 2;
+        }
+
+        //--------TITULOS PESTAÑAS---------//
+        @Override
+        public CharSequence getPageTitle(int position) {
+
+            String section = null;
+
+            switch (position) {
+                case 0:
+                    section = "SECTION 1";
+                    break;
+                case 1:
+                    section = "SECTION 2";
+                    break;
+            }
+            return section;
+        }
+    }
 }
